0e0f4ec09e784abd7fe0fc411b63920505749e65
Sofia383048 <a383048@uach.mx>
2025-10-13 09:52:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ import math
 st.title("mi aplicacion para calcular el area de un circulo")
 radio= st.slider("Selecciona el radio", 0.0, 10.0, 5.0)
 area= math.pi * radio**2
+st.write(f"El area del circulo con radio {radio} es:{area: .2f}")
